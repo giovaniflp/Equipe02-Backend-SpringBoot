2e4829a57711b253b6adb6cb5ca328820cd19bd8
aluno <aluno@ifpe>
2024-03-20 19:33:18 -0300

    Adicionando listagem

--- src/main/java/br/com/ifpe/oxefoodapi/api/cliente/ClienteController.java
+++ src/main/java/br/com/ifpe/oxefoodapi/api/cliente/ClienteController.java
@@ -1,9 +1,13 @@
 package br.com.ifpe.oxefoodapi.api.cliente;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,6 +23,16 @@ public class ClienteController {
 
    @Autowired
    private ClienteService clienteService;
+
+   @GetMapping
+    public List<Cliente> listarTodos() {
+        return clienteService.listarTodos();
+    }
+
+    @GetMapping("/{id}")
+    public Cliente obterPorID(@PathVariable Long id) {
+        return clienteService.obterPorID(id);
+    }
 
    @PostMapping
    public ResponseEntity<Cliente> save(@RequestBody ClienteRequest request) {
--- src/main/java/br/com/ifpe/oxefoodapi/modelo/cliente/ClienteService.java
+++ src/main/java/br/com/ifpe/oxefoodapi/modelo/cliente/ClienteService.java
@@ -3,6 +3,7 @@ package br.com.ifpe.oxefoodapi.modelo.cliente;
 import jakarta.transaction.Transactional;
 
 import java.time.LocalDate;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -10,16 +11,25 @@ import org.springframework.stereotype.Service;
 @Service
 public class ClienteService {
 
-   @Autowired
-   private ClienteRepository repository;
+@Autowired
+private ClienteRepository repository;
 
-   @Transactional
-   public Cliente save(Cliente cliente) {
+@Transactional
+public Cliente save(Cliente cliente) {
 
-       cliente.setHabilitado(Boolean.TRUE);
-       cliente.setVersao(1L);
-       cliente.setDataCriacao(LocalDate.now());
-       return repository.save(cliente);
-   }
+    cliente.setHabilitado(Boolean.TRUE);
+    cliente.setVersao(1L);
+    cliente.setDataCriacao(LocalDate.now());
+    return repository.save(cliente);
+}
+
+public List<Cliente> listarTodos() {
+    return repository.findAll();
+}
+
+public Cliente obterPorID(Long id) {
+    return repository.findById(id).get();
+}
+
 
 }
